cf80f3f81a2569f936300ec2bdf7d2786af1b5f6
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2025-12-09 22:52:46 +0530

    Add login and logout endpoints

--- backend/server.js
+++ backend/server.js
@@ -12,5 +12,17 @@ app.post('/signup', (req, res) => {
         data: {name, email}
     })
 })
+app.post('/login', (req, res) => {
+    const {email, password} = req.body;
+    res.send({
+        message: "Login endpoint working",
+        email
+    })
+})
+app.post('/logout', (req, res) => {
+    res.send({
+        message: "Logout endpoint working"
+    })
+})
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
