74663a85a1472e6ff6f35f4b14ca7d6fae832601
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2025-12-12 10:52:15 +0530

    refactor(server): organize server.js structure so it all looks super neat

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 dotenv.config();
 
-const connectDB = require('./config/db');
-
-const authRoutes = require('./routes/auth');
-
 const app = express();
-const port = process.env.PORT || 8000;
 
 connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.send({ message: 'Workshop Management System' }));
-
+const authRoutes = require('./routes/auth');
+app.get('/', (req, res) => res.send({ message: "Workshop Management System" }));
 app.use('/api/auth', authRoutes);
 
 app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).send({ error: 'Server error' });
+    console.error(err);
+    res.status(500).send({ error: "Server error" });
 });
 
+const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server listening on ${port}`));
