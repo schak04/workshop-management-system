eb80ef8aaf8767bed1387551aa46392e8cafb6b4
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2025-12-12 09:49:36 +0530

    fix: correct typo

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
-    password: {type: String, rquired: true},
+    password: {type: String, required: true},
     role: {type: String, enum: ['admin', 'instructor', 'participant'], default: 'participant'}
 }, {timestamps: true});
 
