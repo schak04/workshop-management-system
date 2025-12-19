4bb5315dd4ab5b2063b033cb96df8e4cca91d4da
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2025-12-19 12:31:07 +0530

    feat(dashboard): update dashboard to only let the admin to create workshops

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,33 +1,38 @@
-import {Link} from 'react-router-dom';
-import {useAuth} from '../context/AuthContext';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 
 export default function Dashboard() {
     const { user } = useAuth();
     if (!user) return null;
 
     return (
-        <div className="p-6">
-            <h1 className="text-2xl font-semibold mb-4">
+        <div className='p-6'>
+            <h1 className='text-2xl font-semibold mb-1'>
                 Welcome, {user.name}
             </h1>
 
-            <p className="mb-6 text-gray-600">
+            <p className='mb-6 text-gray-600'>
                 Role: <strong>{user.role}</strong>
             </p>
 
-            <div className="grid gap-4 max-w-md">
+            <div className='grid gap-4 max-w-md'>
 
-                <Link className="border p-3 rounded" to="/workshops">
+                <Link className='border p-3 rounded' to='/workshops'>
                     View Workshops
                 </Link>
 
+                {user.role === 'admin' && (
+                    <Link className='border p-3 rounded' to='/workshops/create'>
+                        Create Workshop
+                    </Link>
+                )}
+
                 {user.role === 'participant' && (
                     <>
-                        <Link className="border p-3 rounded" to="/certificates">
+                        <Link className='border p-3 rounded' to='/certificates'>
                             My Certificates
                         </Link>
-
-                        <Link className="border p-3 rounded" to="/feedback">
+                        <Link className='border p-3 rounded' to='/feedback'>
                             Submit Feedback
                         </Link>
                     </>
@@ -35,11 +40,10 @@ export default function Dashboard() {
 
                 {(user.role === 'instructor' || user.role === 'admin') && (
                     <>
-                        <Link className="border p-3 rounded" to="/attendance">
+                        <Link className='border p-3 rounded' to='/attendance'>
                             Manage Attendance
                         </Link>
-
-                        <Link className="border p-3 rounded" to="/feedback">
+                        <Link className='border p-3 rounded' to='/feedback'>
                             View Feedback
                         </Link>
                     </>
